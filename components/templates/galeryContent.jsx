599b54ff9f17a0be1457f2c9c0ce1f863ca49982
“Reyhan <rcteenx@gmail.com>
2024-11-06 21:03:11 +0300

    update photo gallery 5

--- components/templates/galeryContent.jsx
+++ components/templates/galeryContent.jsx
@@ -2,7 +2,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const titleStyles = cva(
-  "mx-auto md:p-8 max-w-2xl flex flex-col gap-4 text-center shadow-xl rounded-2xl ",
+  "mx-auto p-4 md:p-8 max-w-2xl flex flex-col gap-4 shadow-xl md:rounded-2xl text-center",
   {
     variants: {
       bgColor: {
@@ -27,7 +27,7 @@ export default function GalleryContent({
   ...props
 }) {
   return (
-    <section className="mx-auto container-p my-4">
+    <section className="mx-auto md:my-4">
       <div className={cn(titleStyles({ bgColor }))}>
         <h2 className="mx-auto inline-block py-1 border-coral-red border-b text-center">
           {h2Title}
--- components/templates/pageContent.jsx
+++ components/templates/pageContent.jsx
@@ -2,7 +2,7 @@ import { cva } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const titleStyles = cva(
-  "mx-auto p-4 md:p-12 max-w-2xl flex flex-col gap-4 text-left shadow-xl rounded-2xl ",
+  "mx-auto p-4 md:p-8 max-w-2xl flex flex-col gap-4 shadow-xl md:rounded-2xl text-left",
   {
     variants: {
       bgColor: {
@@ -22,7 +22,7 @@ const titleStyles = cva(
 
 export default function PageContent({ bgColor, h2Title, children, ...props }) {
   return (
-    <section className="mx-auto container-p my-4">
+    <section className="mx-auto md:my-4">
       <div className={cn(titleStyles({ bgColor }))}>
         <h2 className="mx-auto inline-block py-1 border-coral-red border-b text-center">
           {h2Title}
