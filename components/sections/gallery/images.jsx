67b8bee4a1a425d0ef221477c28a5a65fa649d88
“Reyhan <rcteenx@gmail.com>
2024-11-06 20:51:03 +0300

    update photo gallery 2

--- components/sections/gallery/images.jsx
+++ components/sections/gallery/images.jsx
@@ -6,11 +6,11 @@ export default function ImageGallery({ galeri }) {
       {galeri.categories.map((c, index) => (
         <div key={index}>
           <h3>{c.h3}</h3>
-          <div className="flex flex-wrap gap-4 justify-center ">
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 justify-center ">
             {c.items.map((i, index) => (
               <div
                 key={index}
-                className="p-1 md:p-2 w-44 bg-white rounded-xl transition-transform duration-300 transform hover:scale-150 hover:z-50 hover:bg-purple-600"
+                className="p-1 md:p-2 bg-white rounded-xl transition-transform duration-300 transform hover:scale-150 hover:z-50 hover:bg-purple-600"
               >
                 <a href={`/assets/galeri/${i.src}`}>
                   <img
